75f030abbc72884c1f98fc6a3a68febe6b8dcefe
Nickkodag <53111448+Nickkodag@users.noreply.github.com>
2021-03-14 14:12:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var React= require('react');
+var ReactDOM=require('react-dom');
+//{/* h1 tag use  in here is not part of html but part of jsx(javascript extension)*/}
+//{/*render methodus in react to link between reacr component*/}
+
+ReactDOM.render(
+<>
+<h1> Nilesh Kodag technocal stuff </h1>
+<p>
+    Here are list of Five Netflex series
+</p>
+<ol>
+    <li>Dark as series</li>
+    <li>My First love</li>
+    <li>mr robot</li> 
+</ol>
+</>
+    ,document.getElementById('root')
+    );
